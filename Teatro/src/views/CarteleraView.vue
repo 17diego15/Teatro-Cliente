e6fd5e1ef65576bd60ae2d6cd7c29de0e23bcab6
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-02-08 19:26:57 +0100

    Cartelera completa

--- Teatro/src/views/CarteleraView.vue
+++ Teatro/src/views/CarteleraView.vue
@@ -1,15 +1,40 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+import imagen1 from '../asserts/slider1.jpg';
+import imagen2 from '../asserts/slider2.jpg';
+
+export default defineComponent({
+  name: 'ObrasComponent',
+  setup() {
+    const obras = ref([
+      { id: 1, imagen: imagen1, nombre: 'Obra 1' },
+      { id: 2, imagen: imagen2, nombre: 'Obra 2' },
+      { id: 2, imagen: imagen2, nombre: 'Obra 2' },
+      { id: 2, imagen: imagen2, nombre: 'Obra 2' },
+      { id: 2, imagen: imagen2, nombre: 'Obra 2' },
+      { id: 2, imagen: imagen2, nombre: 'Obra 2' },
+      { id: 2, imagen: imagen2, nombre: 'Obra 2' },
+
+    ]);
+
+    const comprarObra = (id: number) => {
+      window.location.href = `../Paginas/InfoObra.html?id=${id}`;
+    };
+
+    return { obras, comprarObra };
+  }
+});
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="cartelera_section" id="obrasContainer">
+    <div class="cartelera_div" id="obrasContainer">
+      <div v-for="obra in obras" :key="obra.id" class="cartelera_div_img">
+        <img :src="obra.imagen" :alt="'Imagen de ' + obra.nombre">
+        <button class="cartelera_boton1" @click="comprarObra(obra.id)">Comprar</button>
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
