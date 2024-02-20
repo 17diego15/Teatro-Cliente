276138c5b241c646adebcb398dd148c68fed96e1
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-02-20 21:03:59 +0100

    Cartelera con fetch

--- src/views/CarteleraView.vue
+++ src/views/CarteleraView.vue
@@ -1,38 +1,56 @@
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, onMounted } from 'vue';
 
 import obra1 from '../assets/impulso.jpg'
 import obra2 from '../assets/caminoAlZoo.jpg'
 
+interface Obra {
+  obraID: number;
+  titulo: string;
+  director: string;
+  sinopsis: string;
+  duración: string;
+  precio: number;
+  imagen: string;
+}
+
 export default defineComponent({
   name: 'ObrasComponent',
   setup() {
-    const obras = ref([
-      { id: 1, imagen: obra1, nombre: 'Obra 1' },
-      { id: 2, imagen: obra2, nombre: 'Obra 2' },
-      { id: 3, imagen: obra2, nombre: 'Obra 2' },
-      { id: 4, imagen: obra2, nombre: 'Obra 2' },
-      { id: 5, imagen: obra2, nombre: 'Obra 2' },
-      { id: 6, imagen: obra2, nombre: 'Obra 2' },
-      { id: 7, imagen: obra2, nombre: 'Obra 2' },
-
-    ]);
+    const obras = ref<Obra[]>([]);
 
     const comprarObra = (id: number) => {
+      //cambiar metodo
       window.location.href = `/obra/${id}`;
     };
 
+    const cargarObras = async () => {
+      try {
+        const respuesta = await fetch('/api/obras');
+        if (!respuesta.ok) {
+          throw new Error('Error al obtener las obras');
+        }
+        const data: Obra[] = await respuesta.json();
+        obras.value = data;
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    onMounted(() => {
+      cargarObras();
+    });
     return { obras, comprarObra };
   }
 });
+
 </script>
 
 <template>
   <div class="cartelera_section" id="obrasContainer">
     <div class="cartelera_div" id="obrasContainer">
-      <div v-for="obra in obras" :key="obra.id" class="cartelera_div_img">
-        <img :src="obra.imagen" :alt="'Imagen de ' + obra.nombre">
-        <button class="cartelera_boton1" @click="comprarObra(obra.id)">Comprar</button>
+      <div v-for="obra in obras" :key="obra.obraID" class="cartelera_div_img">
+        <img :src="obra.imagen" :alt="'Imagen de ' + obra.titulo">
+        <button class="cartelera_boton1" @click="comprarObra(obra.obraID)">Comprar</button>
       </div>
     </div>
   </div>
