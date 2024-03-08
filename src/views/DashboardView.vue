d7eefd7e0afd8fdf07ad91be11c3b0a857728e50
17diego15 <a24957@svalero.com>
2024-03-08 13:43:31 +0100

    Funciones actualizado

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -28,9 +28,9 @@ interface Usuario {
 interface Funcion {
   funcionID: number;
   obraID: string;
-  salaID: string;
   fecha: string;
   hora: string;
+  disponibilidad: string;
   obra: Obra;
 }
 
@@ -44,6 +44,7 @@ export default defineComponent({
     const route = useRoute();
 
     const obraAEditar = ref<Obra | null>(null);
+    const funcionAEditar = ref<Funcion | null>(null);
     const mostrandoFormularioEdicion = ref(false);
 
     const seccionActiva = ref('obras');
@@ -69,6 +70,18 @@ export default defineComponent({
       mostrandoFormularioEdicion.value = true;
     };
 
+    const crearFuncion = () => {
+      estamosCreando.value = true;
+      funcionAEditar.value = {
+        funcionID: 0,
+        obraID: '',
+        fecha: '',
+        hora: '',
+        disponibilidad: '',
+      };
+      mostrandoFormularioEdicion.value = true;
+    };
+
     const admin = (isAdmin: string | boolean): string => {
       const isAdministrator = typeof isAdmin === 'string' ? isAdmin.toLowerCase() === 'true' : isAdmin;
       return isAdministrator ? 'Es administrador' : 'No es administrador';
@@ -87,6 +100,12 @@ export default defineComponent({
       obraAEditar.value = { ...obra };
       mostrandoFormularioEdicion.value = true;
     };
+
+    const editarFuncion = (funcion: Funcion) => {
+      funcionAEditar.value = { ...funcion };
+      mostrandoFormularioEdicion.value = true;
+    };
+
 
     const cancelarEdicion = () => {
       obraAEditar.value = null;
@@ -117,7 +136,7 @@ export default defineComponent({
         }
 
         console.log('Obra eliminada con éxito');
-        await cargarObras(); 
+        await cargarObras();
       } catch (error) {
         console.error('Error al eliminar la obra:', error);
       }
@@ -137,7 +156,7 @@ export default defineComponent({
       }
     };
 
-    const enviarEdicion = async () => {
+    const enviarEdicionObra = async () => {
       if (obraAEditar.value) {
         const url = estamosCreando.value ? '/api/obras' : `/api/obras/${obraAEditar.value.obraID}`;
         const metodo = estamosCreando.value ? 'POST' : 'PUT';
@@ -157,8 +176,8 @@ export default defineComponent({
 
           console.log('Obra guardada con éxito');
           mostrandoFormularioEdicion.value = false;
-          estamosCreando.value = false; 
-          await cargarObras(); 
+          estamosCreando.value = false;
+          await cargarObras();
         } catch (error) {
           console.error('Error al guardar la obra:', error);
         }
@@ -175,6 +194,53 @@ export default defineComponent({
         funciones.value = data;
       } catch (error) {
         console.error(error);
+      }
+    };
+
+    const enviarEdicionFuncion = async () => {
+      if (funcionAEditar.value) {
+        const url = estamosCreando.value ? '/api/funciones' : `/api/funciones/${funcionAEditar.value.funcionID}`;
+        const metodo = estamosCreando.value ? 'POST' : 'PUT';
+
+        try {
+          const respuesta = await fetch(url, {
+            method: metodo,
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(funcionAEditar.value),
+          });
+
+          if (!respuesta.ok) {
+            throw new Error('Error al enviar la función');
+          }
+
+          console.log('Función enviada con éxito');
+          await cargarFunciones();
+        } catch (error) {
+          console.error('Error al enviar la función:', error);
+        } finally {
+          mostrandoFormularioEdicion.value = false;
+          funcionAEditar.value = null;
+          estamosCreando.value = false;
+        }
+      }
+    };
+
+    const eliminarFuncion = async (id: number) => {
+      try {
+        const respuesta = await fetch(`/api/funcion/${id}`, {
+          method: 'DELETE'
+        });
+
+        if (!respuesta.ok) {
+          throw new Error('Error al eliminar la funcion');
+        }
+
+        console.log('Funcion eliminada con éxito');
+        await cargarFunciones();
+      } catch (error) {
+        console.error('Error al eliminar la funcion:', error);
       }
     };
 
@@ -205,14 +271,18 @@ export default defineComponent({
       mostrarFunciones,
       mostrarUsuarios,
       obraAEditar,
+      funcionAEditar,
       mostrandoFormularioEdicion,
       editarObra,
-      enviarEdicion,
+      editarFuncion,
+      enviarEdicionObra,
+      enviarEdicionFuncion,
       cancelarEdicion,
       añadirActor,
       eliminarActor,
       eliminarObra,
-      crearObra
+      crearObra,
+      eliminarFuncion
     };
   }
 });
@@ -229,7 +299,7 @@ export default defineComponent({
     </div>
     <div class="dashboard_menu_derecha">
       <div v-if="seccionActiva === 'obras' && mostrandoFormularioEdicion && obraAEditar">
-        <form @submit.prevent="enviarEdicion" class="dashboard_menu_container">
+        <form @submit.prevent="enviarEdicionObra" class="dashboard_menu_container">
           <div class="dashboard_put">
             <label for="titulo">Título:</label>
             <input id="titulo" type="text" v-model="obraAEditar.titulo" placeholder="Título" />
@@ -280,21 +350,44 @@ export default defineComponent({
         </div>
       </div>
 
-      <div class="dashboard_contenido_funciones" v-if="seccionActiva === 'funciones'">
-        <div class="dashboard_item" v-for="funcion in funciones" :key="funcion.funcionID">
-          <div class="dashboard_contenido_tabla">{{ funcion.funcionID }}</div>
-          <div class="dashboard_contenido_tabla">{{ funcion.obra.titulo }}</div>
-          <div class="dashboard_contenido_tabla">{{ formatearFecha(funcion.fecha) }}</div>
-          <div class="dashboard_contenido_tabla">{{ funcion.hora }}</div>
-          <div class="dashboard_iconos">
-            <svg class="dashboard_contenido_tabla" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-              <path
-                d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-              <path
-                d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
-            </svg>
+      <div v-if="seccionActiva === 'funciones' && mostrandoFormularioEdicion && funcionAEditar">
+        <form @submit.prevent="enviarEdicionFuncion" class="dashboard_menu_container">
+          <div class="dashboard_put">
+            <label for="fecha">ObraId:</label>
+            <input id="o" type="date" v-model="funcionAEditar.fecha" placeholder="Fecha">
+            <label for="fecha">Fecha:</label>
+            <input id="fecha" type="date" v-model="funcionAEditar.fecha" placeholder="Fecha">
+            <label for="hora">Hora:</label>
+            <input id="hora" type="text" v-model="funcionAEditar.hora" placeholder="Hora">
+            <label for="disponibilidad">Disponibilidad:</label>
+            <input id="disponibilidad" type="text" v-model="funcionAEditar.disponibilidad" placeholder="Disponibilidad">
+          </div>
+          <div class="dashboard_botones">
+            <button type="submit">Guardar cambios</button>
+            <button type="button" @click="cancelarEdicion">Cancelar</button>
+          </div>
+        </form>
+      </div>
+
+      <!-- Lista de Funciones -->
+      <div v-if="seccionActiva === 'funciones' && !mostrandoFormularioEdicion">
+        <div class="dashboard_contenido_funciones">
+          <div class="dashboard_item" v-for="funcion in funciones" :key="funcion.funcionID">
+            <div>{{ funcion.funcionID }}</div>
+            <div>{{ funcion.obra.titulo }}</div>
+            <div>{{ formatearFecha(funcion.fecha) }}</div>
+            <div>{{ funcion.hora }}</div>
+            <div class="dashboard_iconos">
+              <svg @click="editarFuncion(funcion)" class="icono-editar" xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512">
+                <path
+                  d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
+              </svg>
+              <svg @click="eliminarFuncion(funcion.funcionID)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                <path
+                  d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
+              </svg>
+            </div>
           </div>
         </div>
       </div>
