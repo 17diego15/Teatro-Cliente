350f168757cfb883902c873bad6f992f350ed95c
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-03-08 20:21:07 +0100

    Usuarios creado

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -46,18 +46,35 @@ export default defineComponent({
 
     const obraAEditar = ref<Obra | null>(null);
     const funcionAEditar = ref<Funcion | null>(null);
+    const usuarioAEditar = ref<Usuario | null>(null);
     const mostrandoFormularioEdicion = ref(false);
 
     const seccionActiva = ref('obras');
 
-    const mostrarObras = () => { seccionActiva.value = 'obras'; cargarObras(); };
-    const mostrarFunciones = () => { seccionActiva.value = 'funciones'; cargarFunciones() };
-    const mostrarUsuarios = () => { seccionActiva.value = 'usuarios'; cargarUsuarios(); };
+    const mostrarObras = () => {
+      seccionActiva.value = 'obras';
+      mostrandoFormularioEdicion.value = false;
+      obraAEditar.value = null;
+      cargarObras();
+    };
+    const mostrarFunciones = () => {
+      seccionActiva.value = 'funciones';
+      mostrandoFormularioEdicion.value = false;
+      funcionAEditar.value = null;
+      cargarFunciones();
+    };
+
+    const mostrarUsuarios = () => {
+      seccionActiva.value = 'usuarios';
+      mostrandoFormularioEdicion.value = false;
+      usuarioAEditar.value = null;
+      cargarUsuarios();
+    };
 
     const estamosCreando = ref(false);
 
     const crearObra = () => {
-      estamosCreando.value = true;
+      seccionActiva.value = 'obras';
       obraAEditar.value = {
         obraID: 0,
         titulo: '',
@@ -69,10 +86,11 @@ export default defineComponent({
         actores: [],
       };
       mostrandoFormularioEdicion.value = true;
+      estamosCreando.value = true;
     };
 
     const crearFuncion = () => {
-      estamosCreando.value = true;
+      seccionActiva.value = 'funciones';
       funcionAEditar.value = {
         funcionID: 0,
         obraID: 0,
@@ -82,6 +100,7 @@ export default defineComponent({
         disponibilidad: '',
       };
       mostrandoFormularioEdicion.value = true;
+      estamosCreando.value = true;
     };
 
     const admin = (isAdmin: string | boolean): string => {
@@ -108,6 +127,10 @@ export default defineComponent({
       mostrandoFormularioEdicion.value = true;
     };
 
+    const editarUsuario = (usuario: Usuario) => {
+      usuarioAEditar.value = { ...usuario };
+      mostrandoFormularioEdicion.value = true;
+    };
 
     const cancelarEdicion = () => {
       obraAEditar.value = null;
@@ -258,6 +281,56 @@ export default defineComponent({
         console.error(error);
       }
     };
+
+    const enviarEdicionUsuario = async () => {
+      if (usuarioAEditar.value) {
+        const usuarioActualizado = {
+          ...usuarioAEditar.value,
+          isAdmin: usuarioAEditar.value.isAdmin === 'true',
+        };
+        try {
+          const respuesta = await fetch(`/api/usuario/${usuarioActualizado.usuarioID}`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(usuarioActualizado),
+          });
+
+          if (!respuesta.ok) {
+            throw new Error('Error al enviar el usuario');
+          }
+
+          console.log('Usuario enviado con éxito');
+          await cargarUsuarios();
+        } catch (error) {
+          console.error('Error al enviar el usuario:', error);
+        } finally {
+          mostrandoFormularioEdicion.value = false;
+          usuarioAEditar.value = null;
+          estamosCreando.value = false;
+        }
+      }
+    };
+
+    const eliminarUsuario = async (id: number) => {
+      try {
+        const respuesta = await fetch(`/api/usuario/${id}`, {
+          method: 'DELETE'
+        });
+
+        if (!respuesta.ok) {
+          throw new Error('Error al eliminar el usuario');
+        }
+
+        console.log('Usuario eliminada con éxito');
+        await cargarUsuarios();
+      } catch (error) {
+        console.error('Error al eliminar el usuario:', error);
+      }
+    };
+
+
     onMounted(() => {
       cargarObras();
       cargarUsuarios();
@@ -274,15 +347,19 @@ export default defineComponent({
       mostrarUsuarios,
       obraAEditar,
       funcionAEditar,
+      usuarioAEditar,
       mostrandoFormularioEdicion,
       editarObra,
       editarFuncion,
+      editarUsuario,
       enviarEdicionObra,
       enviarEdicionFuncion,
+      enviarEdicionUsuario,
       cancelarEdicion,
       añadirActor,
       eliminarActor,
       eliminarObra,
+      eliminarUsuario,
       crearObra,
       crearFuncion,
       eliminarFuncion
@@ -375,7 +452,6 @@ export default defineComponent({
         </form>
       </div>
 
-      <!-- Lista de Funciones -->
       <div v-if="seccionActiva === 'funciones' && !mostrandoFormularioEdicion">
         <div class="dashboard_contenido_funciones">
           <div class="dashboard_item" v-for="funcion in funciones" :key="funcion.funcionID">
@@ -398,21 +474,40 @@ export default defineComponent({
         </div>
       </div>
 
-      <div class="dashboard_contenido_usuarios" v-if="seccionActiva === 'usuarios'">
-        <div class="dashboard_item" v-for="usuario in usuarios" :key="usuario.usuarioID">
-          <div class="dashboard_contenido_tabla">{{ usuario.usuarioID }}</div>
-          <div class="dashboard_contenido_tabla">{{ usuario.nombre }}</div>
-          <div class="dashboard_contenido_tabla">{{ usuario.nombreUsuario }}</div>
-          <div class="dashboard_contenido_tabla">{{ admin(usuario.isAdmin) }}</div>
-          <div class="dashboard_iconos">
-            <svg class="dashboard_contenido_tabla" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-              <path
-                d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-              <path
-                d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
-            </svg>
+      <div v-if="seccionActiva === 'usuarios' && mostrandoFormularioEdicion && usuarioAEditar">
+        <form @submit.prevent="enviarEdicionUsuario" class="dashboard_menu_container">
+          <div class="dashboard_put">
+            <label for="isAdmin">Es Admin:</label>
+            <select id="isAdmin" v-model="usuarioAEditar.isAdmin">
+              <option value="false">No</option>
+              <option value="true">Sí</option>
+            </select>
+          </div>
+          <div class="dashboard_botones">
+            <button type="submit">Guardar cambios</button>
+            <button type="button" @click="cancelarEdicion">Cancelar</button>
+          </div>
+        </form>
+      </div>
+
+      <div v-if="seccionActiva === 'usuarios' && !mostrandoFormularioEdicion">
+        <div class="dashboard_contenido_usuarios">
+          <div class="dashboard_item" v-for="usuario in usuarios" :key="usuario.usuarioID">
+            <div class="dashboard_contenido_tabla">{{ usuario.usuarioID }}</div>
+            <div class="dashboard_contenido_tabla">{{ usuario.nombre }}</div>
+            <div class="dashboard_contenido_tabla">{{ usuario.nombreUsuario }}</div>
+            <div class="dashboard_contenido_tabla">{{ admin(usuario.isAdmin) }}</div>
+            <div class="dashboard_iconos">
+              <svg @click="editarUsuario(usuario)" class="dashboard_contenido_tabla" xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512">
+                <path
+                  d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
+              </svg>
+              <svg @click="eliminarUsuario(usuario.usuarioID)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                <path
+                  d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
+              </svg>
+            </div>
           </div>
         </div>
       </div>
