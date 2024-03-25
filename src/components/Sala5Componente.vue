96e4da9aa395bda5009c2fb62952feae8fed50d2
17diego15 <a24957@svalero.com>
2024-03-25 17:39:29 +0100

    Corregido error sala

--- src/components/Sala5Componente.vue
+++ src/components/Sala5Componente.vue
@@ -1,14 +1,27 @@
 <script lang="ts">
-export default {
-    name: 'Sala1',
+import { defineComponent } from 'vue';
+import type { PropType } from 'vue';
+
+export default defineComponent({
+    name: 'Sala5',
     props: {
-        rows: Array,
-        getColsForRow: Function,
-        getSeatColor: Function,
-        toggleSeatColor: Function,
+        rows: Array as PropType<number[]>,
+        getColsForRow: {
+            type: Function as PropType<(row: number) => number[]>,
+            required: true
+        },
+        getSeatColor: {
+            type: Function as PropType<(seat: { row: number; col: number }) => string>,
+            required: true
+        },
+        toggleSeatColor: {
+            type: Function as PropType<(seat: { row: number; col: number }) => void>,
+            required: true
+        },
     }
-}
+});
 </script>
+
 <template>
     <svg width="40%" viewBox="-60 0 200 120" class="sala_svg" preserveAspectRatio="xMidYMid meet">
         <g v-for="row in rows" :key="row">
@@ -16,8 +29,9 @@ export default {
                 <rect :x="col * 10 - 10" :y="row * 10 - 10" width="9" height="9" :fill="getSeatColor({ row, col })"
                     @click="() => toggleSeatColor({ row, col })" />
                 <text :x="col * 10 - (col > 7 && row >= 4 ? 7.5 : 7.5)" :y="row * 10 - 5" fill="black" font-size="3"
-                    @click="() => toggleSeatColor({ row, col })">{{ row }}-{{ col > 7 && row >= 4 ? col - 1 : col
-                    }}</text>
+                    @click="() => toggleSeatColor({ row, col })">
+                    {{ row }}-{{ col > 7 && row >= 4 ? col - 1 : col }}
+                </text>
             </g>
         </g>
     </svg>
